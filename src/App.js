e3a602a8d20b16f7d19b6608758b41703f51c090
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-01 02:09:09 +0530

    about to build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
-
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between p-4'>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+        <div>
+            <button className='uppercase pr-6 text-white'>Sign In</button>
+            <button className='bg-red-600 px-6 py-2 rounded text-white uppercase'>Sign Up</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
