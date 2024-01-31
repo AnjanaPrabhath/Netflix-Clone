878fd6141cc62414e7c602f03bf4a97c3aabc9ac
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-01 00:54:13 +0530

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <p>This Is Hero</p>
+        <p>This Is Hero is updated</p>
+        <h3>normal topic there</h3>
+        <img src="" alt="little image goes here" />
     </div>
   )
 }
