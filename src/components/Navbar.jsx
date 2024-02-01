dee73e5d6f0489922628620d00515b81f5a5c728
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-02 01:26:57 +0530

    main completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-4'>
+    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
         <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
         <div>
             <button className='uppercase pr-6 text-white'>Sign In</button>
