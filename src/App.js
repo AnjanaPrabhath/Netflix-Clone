f4cfb74404510b8f944bef4b5f330644e06ba5c9
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-03 10:46:39 +0530

    AuthContextProvider added to the App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-      </Routes>
-    </div>
+    <>
+      <AuthContextProvider>
+
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+        
+      </AuthContextProvider>
+    </>
   );
 }
 
