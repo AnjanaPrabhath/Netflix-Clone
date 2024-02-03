b437a218e487e1bb4b02a77a9fac265f082b07ef
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-03 12:35:20 +0530

    incomplete AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,8 +1,18 @@
 import {createContext, useContext, useEffect, useState} from "react"
+import { Auth } from "../firebase"
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+} from 'firebase/auth'
 
 const AuthContext = createContext()
 
 export function AuthContextProvider({children}){
+
+    const [] = useState({})
+
     return(
         <AuthContext.Provider>
             {children}
