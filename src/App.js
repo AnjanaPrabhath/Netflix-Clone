b4a3a004df4c9152bc591caacb2cc67986defdc3
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-06 15:32:19 +0530

    login registration and account setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import { AuthContextProvider } from './context/AuthContext';
+import SignUp from './Pages/SignUp';
+import Login from './Pages/Login';
+import Account from './Pages/Account';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/signup' element={<SignUp/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/account' element={<Account/>} />
         </Routes>
         
       </AuthContextProvider>
