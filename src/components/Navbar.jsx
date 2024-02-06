3cfb984e1e7cbde8454a6d667560f62f2e008677
AnjanaPrabhath <prabhathxdissanayake@gmail.com>
2024-02-06 15:39:43 +0530

    pages linked with nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -7,14 +8,20 @@ const Navbar = () => {
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
 
       {/* logo or web Name */}
-
+      <Link to='/'>
         <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>
           NETFLIX</h1>
+      </Link>
 
       {/* buttons right side */}
         <div>
+          <Link to='/login'>
             <button className='uppercase pr-6 text-white'>Sign In</button>
+          </Link>
+
+          <Link to='/signup'>
             <button className='bg-red-600 px-6 py-2 rounded text-white uppercase'>Sign Up</button>
+          </Link>
         </div>
         
     </div>
